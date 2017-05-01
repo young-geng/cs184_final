35d45f14daa9c16ad1821470f175ada0b70800c7
young-geng <young.gengxy@gmail.com>
2017-04-30 22:05:04 -0700

    problems

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from ball_pivot import *
 
 if __name__ == '__main__':
 
-    plydata = PlyData.read('data/bunny.ply')
+    plydata = PlyData.read('data/dart.ply')
     vs = build_vertex_set_ply(plydata)
 
-    print seed_triangle(0.0003, vs)
+    mesh = pivot_ball(vs, 8.6375)
+    write_collada(vs.vertices, vs.normals, np.array(mesh.faces.keys()), 'temp/dart.dae')
